d5da5685a6882cf31fba453e5aad218c2a31ae04
Shuai <bfshm@qq.com>
2019-10-14 14:46:04 +0800

    update

--- app/routes.py
+++ app/routes.py
@@ -90,7 +90,7 @@ def login():
     '''
     redirect to github login page
     '''
-    return authorize(redirect_uri="/neo3/")
+    return authorize(redirect_uri="https://neowish.ngd.network/neo3/")
 
 
 @app.route('/api/github/callback')
---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git pull
+sudo docker rm -f neo3-backend
+sudo docker rmi neo3-back
+sudo docker build -t neo3-back .
+sudo docker run -it  --name=neo3-backend  --net=host neo3-back
