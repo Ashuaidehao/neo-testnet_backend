c4b5ba5c623054a55bbfdd9ceebe377857adb33d
Shuai <bfshm@qq.com>
2019-10-16 15:02:19 +0800

    update

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ git pull
 sudo docker rm -f neo3-backend
 sudo docker rmi neo3-back
 sudo docker build -t neo3-back .
-sudo docker run -it  --name=neo3-backend  --net=host neo3-back
+sudo docker run -d --name=neo3-backend  --net=host neo3-back
